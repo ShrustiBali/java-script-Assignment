a8350fd05de20cdfad92a451ab8007e155c0cd4e
ShrustiBali <114916812+ShrustiBali@users.noreply.github.com>
2022-10-06 16:43:21 +0530

    Add files via upload

---
+++ 4_day3.js
@@ -0,0 +1,19 @@
+var Object = {
+"India": { "Delhi": ["new Delhi", "North Delhi"],
+"Kerala": ["Thiruvananthapuram", "Palakkad"],
+"Goa": ["North Goa", "South Goa"],
+},
+"Australia": {
+"South Australia": ["Dunstan", "Mitchell"],
+"Victoria": ["Altona", "Euroa"]
+},
+ "New zeland": {
+"Alberta": ["Acadia", "Bighorn"],
+"Columbia": ["Washington", ""]
+},
+"England":{
+"London":["Birmingham","Bath"],
+
+}
+}
+console.log( Object);
